68da334c45a9d09a9daa824af8746878e811631d
Ryan <ryan.leichty@gmail.com>
2022-03-13 23:09:39 -0500

    Update layout of Pet component

--- src/Pet.js
+++ src/Pet.js
@@ -1,10 +1,19 @@
-const Pet = ({ name, animal, breed }) => {
+const Pet = ({ name, animal, breed, images, location, id }) => {
+  let hero = 'http://pets-images.dev-apis.com/pets/none.jpg';
+  if (images.length) {
+    hero = images[0];
+  }
+
   return (
-    <div>
-      <h2>{name}</h2>
-      <h3>{animal}</h3>
-      <h3>{breed}</h3>
-    </div>
+    <a href={`/details/${id}`} className='pet'>
+      <div className='image-container'>
+        <img src={hero} alt={name} />
+      </div>
+      <div className='info'>
+        <h1>{name}</h1>
+        <h2>{`${animal} — ${breed} — ${location}`}</h2>
+      </div>
+    </a>
   );
 };
 
